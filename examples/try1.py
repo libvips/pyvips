53fddee49e2083f7fb4c5e14fdb4fbefb3a5b31e
John Cupitt <jcupitt@gmail.com>
2017-08-06 09:58:10 +0100

    add gvalue tests
    though image array has problems, strange
    
    might need more refs on read?

--- examples/try1.py
+++ examples/try1.py
@@ -1,88 +1,15 @@
 #!/usr/bin/env python
 
-import Vips
-
-print 'test gvalue bool'
-gv = Vips.GValue()
-gv.init(Vips.GValue.gbool_type)
-gv.set(True)
-value = gv.get()
-print 'gvalue =', value
-gv.set(False)
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue int'
-gv = Vips.GValue()
-gv.init(Vips.GValue.gint_type)
-gv.set(12)
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue double'
-gv = Vips.GValue()
-gv.init(Vips.GValue.gdouble_type)
-gv.set(3.1415)
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue enum'
-Vips.vips_lib.vips_interpretation_get_type()
-interpretation_gtype = Vips.gobject_lib.g_type_from_name('VipsInterpretation')
-print 'interpretation_gtype =', interpretation_gtype
-gv = Vips.GValue()
-gv.init(interpretation_gtype)
-gv.set('xyz')
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue flags'
-Vips.vips_lib.vips_operation_flags_get_type()
-operationflags_gtype = Vips.gobject_lib.g_type_from_name('VipsOperationFlags')
-print 'operationflags_gtype =', operationflags_gtype
-gv = Vips.GValue()
-gv.init(operationflags_gtype)
-gv.set(12)
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue str'
-gv = Vips.GValue()
-gv.init(Vips.GValue.gstr_type)
-gv.set('banana')
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue array int'
-gv = Vips.GValue()
-gv.init(Vips.GValue.array_int_type)
-gv.set([1, 2, 3])
-value = gv.get()
-print 'gvalue =', value
-print ''
-
-print 'test gvalue array double'
-gv = Vips.GValue()
-gv.init(Vips.GValue.array_double_type)
-gv.set([1.1, 2.2, 3.3])
-value = gv.get()
-print 'gvalue =', value
-print ''
+import pyvips
 
 print 'test Image'
-image = Vips.Image.new_from_file('/home/john/pics/k2.jpg')
+image = pyvips.Image.new_from_file('/home/john/pics/k2.jpg')
 print 'image =', image
 print 'image.width =', image.width
 print ''
 
 print 'test Operation'
-image2 = Vips.Operation.call('embed', image, 1, 2, 3, 4, extend = 'copy')
+image2 = pyvips.Operation.call('embed', image, 1, 2, 3, 4, extend = 'copy')
 print 'image2 =', image2
 print ''
 
---
+++ pyvips/tests/test_gvalue.py
@@ -0,0 +1,112 @@
+# vim: set fileencoding=utf-8 :
+
+from helpers import *
+
+class TestGValue(PyvipsTester):
+    def test_bool(self):
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.gbool_type)
+        gv.set(True)
+        value = gv.get()
+        self.assertEqual(value, True)
+
+        gv.set(False)
+        value = gv.get()
+        self.assertEqual(value, False)
+
+    def test_int(self):
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.gint_type)
+        gv.set(12)
+        value = gv.get()
+        self.assertEqual(value, 12)
+
+    def test_double(self):
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.gdouble_type)
+        gv.set(3.1415)
+        value = gv.get()
+        self.assertEqual(value, 3.1415)
+
+    def test_enum(self):
+        # the Interpretation enum is created when the first image is made --
+        # make it ourselves in case we are run before the first image
+        pyvips.vips_lib.vips_interpretation_get_type()
+        interpretation_gtype = pyvips.gobject_lib. \
+            g_type_from_name('VipsInterpretation')
+        gv = pyvips.GValue()
+        gv.init(interpretation_gtype)
+        gv.set('xyz')
+        value = gv.get()
+        self.assertEqual(value, 'xyz')
+
+    def test_flags(self):
+        # the OperationFlags enum is created when the first op is made --
+        # make it ourselves in case we are run before that
+        pyvips.vips_lib.vips_operation_flags_get_type()
+        operationflags_gtype = pyvips.gobject_lib. \
+            g_type_from_name('VipsOperationFlags')
+        gv = pyvips.GValue()
+        gv.init(operationflags_gtype)
+        gv.set(12)
+        value = gv.get()
+        self.assertEqual(value, 12)
+
+    def test_string(self):
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.gstr_type)
+        gv.set('banana')
+        value = gv.get()
+        self.assertEqual(value, 'banana')
+
+    def test_array_int(self):
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.array_int_type)
+        gv.set([1, 2, 3])
+        value = gv.get()
+        self.assertAlmostEqualObjects(value, [1, 2, 3])
+
+    def test_array_double(self):
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.array_double_type)
+        gv.set([1.1, 2.1, 3.1])
+        value = gv.get()
+        self.assertAlmostEqualObjects(value, [1.1, 2.1, 3.1])
+
+    def test_image(self):
+        image = pyvips.Image.new_from_file(JPEG_FILE)
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.image_type)
+        gv.set(image)
+        value = gv.get()
+        self.assertEqual(value, image)
+
+    """
+
+    this causes a lot of warnings! investigate
+    
+    perhaps the vips r, g, b images are GCd too early?
+
+    def test_array_image(self):
+        image = pyvips.Image.new_from_file(JPEG_FILE)
+        r, g, b = image.bandsplit()
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.array_image_type)
+        gv.set([r, g, b])
+        value = gv.get()
+        self.assertEqual(value, [r, g, b])
+
+    """
+
+    def test_blob(self):
+        with open(JPEG_FILE, 'rb') as f:
+            blob = f.read()
+        gv = pyvips.GValue()
+        gv.init(pyvips.GValue.blob_type)
+        gv.set(blob)
+        value = gv.get()
+        self.assertEqual(value, blob)
+
+if __name__ == '__main__':
+    unittest.main()
+
