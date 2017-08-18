e0fa7a64343ff271b9dd63bdb14ce5a48b4888bc
Kleis Auke Wolthuizen <github@kleisauke.nl>
2017-08-18 13:12:09 +0200

    Read the Docs Sphinx theme

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,7 @@
 
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -90,7 +91,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -114,11 +116,6 @@ html_sidebars = {
         'navigation.html',
         'searchbox.html',
     ]
-}
-
-html_theme_options = {
-    'github_user': 'jcupitt',
-    'github_repo': 'pyvips',
 }
 
 
