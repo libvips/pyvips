84abb2ca876c45c55dc9b10b3ad1a87e20fd7e36
John Cupitt <jcupitt@gmail.com>
2018-10-06 12:41:23 +0100

    improve the pil/numpy example

--- examples/pil-numpy-pyvips.py
+++ examples/pil-numpy-pyvips.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import sys
 import time
@@ -26,7 +26,6 @@ format_to_dtype = {
 }
 
 # map np dtypes to vips
-# oh dear str lookup ... is there a better way?
 dtype_to_format = {
     'uint8': 'uchar',
     'int8': 'char',
@@ -40,33 +39,41 @@ dtype_to_format = {
     'complex128': 'dpcomplex',
 }
 
+# numpy array to vips image
+def numpy2vips(a):
+    height, width, bands = a.shape
+    linear = a.reshape(width * height * bands)
+    vi = pyvips.Image.new_from_memory(linear.data, width, height, bands,
+                                      dtype_to_format[str(a.dtype)])
+    return vi
+
+# vips image to numpy array
+def vips2numpy(vi):
+    return np.ndarray(buffer=vi.write_to_memory(),
+                      dtype=format_to_dtype[vi.format],
+                      shape=[vi.height, vi.width, vi.bands])
+
 # load with PIL
 start_pillow = time.time()
 pillow_img = np.asarray(Image.open(sys.argv[1]))
-print('Pillow Time:', time.time()-start_pillow)
-print('original shape', pillow_img.shape)
+print('Pillow Time:', time.time() - start_pillow)
+print('pil shape', pillow_img.shape)
 
 # load with vips to a memory array
 start_vips = time.time()
-img = pyvips.Image.new_from_file(sys.argv[1], access='sequential')
-mem_img = img.write_to_memory()
+img = pyvips.Image.new_from_file(sys.argv[1])
+np_3d = vips2numpy(img)
 
-# then make a numpy array from that buffer object
-np_3d = np.ndarray(buffer=mem_img,
-                   dtype=format_to_dtype[img.format],
-                   shape=[img.height, img.width, img.bands])
-
-print('Vips Time:', time.time()-start_vips)
-print('final shape', np_3d.shape)
-
-# verify we have the same result
-print('Sum of the Differences:', np.sum(np_3d-pillow_img))
+print('Vips Time:', time.time() - start_vips)
+print('vips shape', np_3d.shape)
 
 # make a vips image from the numpy array
-height, width, bands = np_3d.shape
-linear = np_3d.reshape(width * height * bands)
-vi = pyvips.Image.new_from_memory(linear.data, width, height, bands,
-                                  dtype_to_format[str(np_3d.dtype)])
+vi = numpy2vips(pillow_img)
+
+# verify we have the same result
+# this can be non-zero for formats like jpg if the two libraries are using
+# different libjpg versions ... try with png instead
+print('Average pil/vips difference:', (vi - img).avg())
 
 # and write back to disc for checking
 vi.write_to_file(sys.argv[2])
