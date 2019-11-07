954468a9bfe03d77c84943a7c2d66137553f5861
John Cupitt <jcupitt@gmail.com>
2019-11-07 16:56:03 +0000

    fix some flake8 warnings

--- examples/watermark.py
+++ examples/watermark.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
- 
+
 import sys
 import pyvips
- 
+
 im = pyvips.Image.new_from_file(sys.argv[1], access="sequential")
- 
+
 text = pyvips.Image.text(sys.argv[3], width=500, dpi=300, align="centre")
 
 # we make an overlay with a solid colour in the main image bands, and a faded
 # version of the text mask as the overlay
 
-# drop any alpha 
+# drop any alpha
 if im.hasalpha():
-    no_alpha = im.extract_band(0, n=im.bands - 1) 
+    no_alpha = im.extract_band(0, n=im.bands - 1)
 else:
     no_alpha = im
 
