ed97413d6ad7b45fbae1280fd5050d75a8e4ad79
John Cupitt <jcupitt@gmail.com>
2022-03-30 17:49:29 +0100

    update text watermark example

--- examples/watermark.py
+++ examples/watermark.py
@@ -5,29 +5,20 @@ import pyvips
 
 im = pyvips.Image.new_from_file(sys.argv[1], access="sequential")
 
-text = pyvips.Image.text(sys.argv[3], width=500, dpi=300, align="centre")
+text = pyvips.Image.text(f"<span color=\"red\">{sys.argv[3]}</span>",
+    width=500, dpi=300, align="centre", rgba=True)
 
-# we make an overlay with a solid colour in the main image bands, and a faded
-# version of the text mask as the overlay
+# scale the alpha down to make the text semi-transparent
+text = (text * [1, 1, 1, 0.3]).cast("uchar")
 
-# drop any alpha
-if im.hasalpha():
-    no_alpha = im.extract_band(0, n=im.bands - 1)
-else:
-    no_alpha = im
+text = text.rotate(45)
 
-# colours have four parts in cmyk images
-if im.bands == 4:
-    text_colour = [0, 255, 0, 0]
-elif im.bands == 3:
-    text_colour = [255, 0, 0]
-else:
-    text_colour = 255
+# tile to the size of the image
+text = text.embed(100, 100, text.width + 200, text.width + 200)
+text = text.replicate(1 + im.width / text.width, 1 + im.height / text.height)
+text = text.crop(0, 0, im.width, im.height)
 
-overlay = no_alpha.new_from_image(text_colour)
-overlay = overlay.bandjoin((text * 0.5).cast("uchar"))
-
-# position overlay at the bottom left, with a margin
-im = im.composite(overlay, "over", x=100, y=im.height - text.height - 100)
+# composite the two layers
+im = im.composite(text, "over")
 
 im.write_to_file(sys.argv[2])
