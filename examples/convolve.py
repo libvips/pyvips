928e2d7957929d1dae81c1cbc0df8f8ddc24fd5e
Kirk Martinez <km@ecs.soton.ac.uk>
2023-01-08 15:43:05 +0000

    a couple of pyvips examples (#369)
    * new convolution example
    
    * draft example thumbnail code to process whole directory
    
    * example thumbnail code to process whole directory
    
    * example thumbnail code example proofread
    
    * example thumbnail code example cleaned up
    
    * new convolution example cleaned up a bit
    
    * example thumbnail code example cleaned up

---
+++ examples/convolve.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" example pyvips code to convolve an image with a 3x3 mask
+
+args: inputfile outputfile
+no error checking for simplicity
+Notes: the convolution mask can be other sizes,
+here we use a scale of 1 (no scaling - see libvips conv docs)
+https://www.libvips.org/API/current/libvips-convolution.html#vips-conv
+and offset of 128 so zero output is mid-grey
+"""
+import pyvips
+import sys
+
+image = pyvips.Image.new_from_file(sys.argv[1], access='sequential')
+mask = pyvips.Image.new_from_array([[1, 1, 1],
+                                    [1, -8, 1],
+                                    [1, 1, 1]
+                                   ], scale=1, offset=128)
+image = image.conv(mask, precision='integer')
+image.write_to_file(sys.argv[2])
