13d45941a0192c7f5f01a06e6f5e0e0b6831c854
John Cupitt <jcupitt@gmail.com>
2023-02-11 18:41:26 +0000

    update animation examples

--- examples/join-animation.py
+++ examples/join-animation.py
@@ -3,16 +3,20 @@
 import sys
 import pyvips
 
+if len(sys.argv) < 3:
+    print(f"usage: {sys.argv[0]} output-image frame1 frame2 ...")
+    sys.exit(1)
+
 # the input images
 # assume these are all the same size
 images = [pyvips.Image.new_from_file(filename, access="sequential")
           for filename in sys.argv[2:]]
 
-# frame delays are in milliseconds
-delay_array = [300] * len(images)
+animation = images[0].pagejoin(images[1:])
 
-animation = pyvips.Image.arrayjoin(images, across=1)
-animation.set_type(pyvips.GValue.gint_type, "page-height", images[0].height)
+# frame delays are in milliseconds ... 300 is pretty slow!
+delay_array = [300] * len(images)
 animation.set_type(pyvips.GValue.array_int_type, "delay", delay_array)
+
 print(f"writing {sys.argv[1]} ...")
 animation.write_to_file(sys.argv[1])
--- examples/split-animation.py
+++ examples/split-animation.py
@@ -3,16 +3,18 @@
 import sys
 import pyvips
 
+if len(sys.argv) != 3:
+    print(f"usage: {sys.argv[0]} output-directory animated-image")
+    sys.exit(1)
+
 # load the animation, chop into pages
 image = pyvips.Image.new_from_file(sys.argv[2], n=-1, access="sequential")
-page_height = image.get("page-height")
-n_pages = image.get("n-pages")
+
 delay = image.get("delay")
 print(f"delay array = {delay}")
 
-pages = [image.crop(0, page_number * page_height, image.width, page_height)
-         for page_number in range(0, n_pages)]
-print(f"writing frames to {sys.argv[1]} ...")
+pages = image.pagesplit()
+print(f"writing frames to {sys.argv[1]}/ ...")
 for page_number in range(len(pages)):
     filename = f"{sys.argv[1]}/frame-{page_number:04}.tif"
     pages[page_number].write_to_file(filename)
