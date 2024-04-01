2437e4d6af715ad2fd20e5ed34757a1b08a659ec
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-04-01 12:57:08 +0200

    Fix flake8 warnings (#460)

--- pyvips/pyvips_build.py
+++ pyvips/pyvips_build.py
@@ -4,7 +4,7 @@ import pkgconfig
 from cffi import FFI
 
 # we must have the vips package to be able to do anything
-if not pkgconfig.exists('vips'): 
+if not pkgconfig.exists('vips'):
     raise Exception('unable to find pkg-config package "vips"')
 if pkgconfig.installed('vips', '< 8.2'):
     raise Exception('pkg-config "vips" is too old -- need libvips 8.2 or later')
@@ -14,7 +14,7 @@ ffibuilder = FFI()
 ffibuilder.set_source("_libvips",
     r""" 
         #include <vips/vips.h>
-    """, 
+    """,
     **pkgconfig.parse('vips'))
 
 # pkgconfig 1.5+ has modversion ... otherwise, use a small shim
@@ -38,12 +38,13 @@ features = {
     'api': True,
 }
 
+
 import vdecls
 
 # handy for debugging
-#with open('vips-source.txt','w') as f:
-#    c = vdecls.cdefs(features)
-#    f.write(c)
+# with open('vips-source.txt','w') as f:
+#     c = vdecls.cdefs(features)
+#     f.write(c)
 
 ffibuilder.cdef(vdecls.cdefs(features))
 
