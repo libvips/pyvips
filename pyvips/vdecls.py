4063253a75085cb289906554c65aed6a809e90fe
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-03-12 12:54:16 +0100

    Add a declaration for `vips_shutdown()`
    Handy for the leak checker.

--- pyvips/vdecls.py
+++ pyvips/vdecls.py
@@ -75,6 +75,8 @@ def cdefs(features):
         typedef void* (*VipsTypeMap2Fn) (GType type, void* a, void* b);
         void* vips_type_map (GType base, void* fn, void* a, void* b);
 
+        void vips_shutdown (void);
+
         const char* vips_error_buffer (void);
         void vips_error_clear (void);
         void vips_error_freeze (void);
