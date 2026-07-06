ba7974bbf7b514a800b28eddcefb89b8979b5616
NewUserHa <32261870+NewUserHa@users.noreply.github.com>
2026-07-06 21:46:42 +0800

    Document: fix doc of `get_n_pages()` to keep correct with libvips document (#567)
    * Update vimage.py
    
    * Update vimage.py
    
    * Update vimage.py

--- pyvips/vimage.py
+++ pyvips/vimage.py
@@ -1673,8 +1673,8 @@ class Image(pyvips.VipsObject, metaclass=ImageType):
     def get_n_pages(self):
         """Get the number of pages in an image file, or 1.
 
-        This is the number of pages in the file the image was loaded from, not
-        the number of pages in the image.
+        This is the number of pages in the image file, not the number of pages
+        that have been loaded into image.
 
         To get the number of pages in an image, divide the image height by
         the page height.
